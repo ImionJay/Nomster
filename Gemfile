e8bb036d25580c0475b6669442b80c15e7ff16e2
jesse w <imionjay@gmail.com>
2017-05-13 01:36:28 +0000

    stuck somewhere

--- Gemfile
+++ Gemfile
@@ -66,5 +66,6 @@ end
 
 gem 'simple_form'
 
-gem "font-awesome-rails"
+gem 'font-awesome-rails'
 
+gem 'devise'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action.mailer.default_url_options = { host: 'https://firehoseproject-imionjay.c9users.io' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

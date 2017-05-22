25413425d551cec01995e1d49fb0e1fb5cce8d84
jesse w <imionjay@gmail.com>
2017-05-22 19:14:34 +0000

    Correcting some tiny mistakes

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,8 @@ class Place < ApplicationRecord
     belongs_to :user
     geocoded_by :address
     after_validation :geocode
+    
+    validates :name, presence: true
     validates :name, :length => { minimum:4 },:presence => true
     validates :address, :length => { minimum:4 },:presence => true
     validates :description, :presence => true

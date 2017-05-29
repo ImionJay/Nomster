0d9f66db3fc07b569ec6c4c7a42f8947d03b6e91
jesse w <imionjay@gmail.com>
2017-05-29 17:01:25 +0000

    Changing some routes

--- Gemfile
+++ Gemfile
@@ -73,3 +73,5 @@ gem 'devise'
 gem "geocoder"
 
 gem "figaro"
+
+gem 'carrierwave', '~> 1.0'
--- db/migrate/20170526210927_create_photos.rb
+++ db/migrate/20170526210927_create_photos.rb
@@ -1,8 +1,6 @@
 class CreatePhotos < ActiveRecord::Migration[5.0]
   def change
     create_table :photos do |t|
-            t.caption :place_id 
-            t.caption :photo_id
       t.timestamps
     end
   end

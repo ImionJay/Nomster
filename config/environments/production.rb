a931774e211dc380996875e4c0f587491c59d6ba
jesse w <imionjay@gmail.com>
2017-06-01 22:31:42 +0000

    email comments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   }
 
   # more code here
-end
+
   
   config.action_mailer.default_url_options = { host: 'firehoseproject-imionjay.c9users.io' }
   # Settings specified here will take precedence over those in config/application.rb.

8d8e66b44bfa035f97027ff93272d356309a56a3
jesse w <imionjay@gmail.com>
2017-06-02 22:14:43 +0000

    fixes to photo button

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,7 @@ class Photo < ApplicationRecord
     belongs_to :user
     belongs_to :place
     
+    mount_uploader :picture, PictureUploader
 end
 
 # One place can have many photos, but photos can't have many places.
